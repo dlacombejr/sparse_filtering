14c4cc8b355873f19cefe31eb7e2c78b37dd007b
dan <dlacombe2013@fau.edu>
2015-10-22 13:36:55 -0400

    adding boto functionality

--- test.py
+++ test.py
@@ -187,7 +187,7 @@ def main():
         my_bucket = 'danbuckettest'
         bucket = s3.get_bucket(my_bucket)
         k = Key(bucket)
-        directory_format = "/saved/%4d-%02d-%02d_%02dh%02dm%02ds"
+        directory_format = "~/saved/%4d-%02d-%02d_%02dh%02dm%02ds"
         directory_name = directory_format % time.localtime()[0:6]
         os.mkdir(directory_name)
 
